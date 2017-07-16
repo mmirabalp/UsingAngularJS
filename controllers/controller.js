5b225a0155c8ce56bfda82ad1bcc0850f1857c97
Mawel Mirabal <mmirabalp@gmail.com>
2017-07-16 03:45:45 -0700

    Two ways data bindging and ng-model

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,6 +5,22 @@
 */
 var app = angular.module('myApp', []);
 app.controller('displayCtrl', ['$scope', function ($scope) {
-	$scope.nombre = "Mawel";
+	$scope.names = "Mawel";
+	$scope.newComment = {};
+	$scope.comments = [
+		{
+			comment : "Good Work",
+			userName: "Mawel"
+		},
+		{	
+			comment : "Bad Work",
+			userName: "Julio"
+		},
+		
+	];
+	$scope.addComments = function(){
+		$scope.comments.push($scope.newComment);
+		$scope.newComment = {};
+	}
 	
 }])
